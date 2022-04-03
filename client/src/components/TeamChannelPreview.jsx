262519287a4618aebc991cf6a7bdc33d7458790b
Сергей Ашарин <kalliak2014@yandex.ru>
2022-04-03 22:50:31 +0300

    fix errors. Sign In, Sign Up, Logout finished

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -12,14 +12,15 @@ const TeamChannelPreview = ({ channel, type }) => {
 
   const DirectPreview = () => {
     const members = Object.values(channel.state.members).filter(
-      ({ user }) => user.id != client.userID
+      ({ user }) => user.id !== client.userID
     );
+    console.log(members[0]);
 
     return (
       <div className="channel-preview__item single">
         <Avatar
           image={members[0]?.user?.image}
-          name={members[0]?.user?.fullName}
+          name={members[0]?.user?.fullName || members[0]?.user?.id}
           size={24}
         />
         <p>{members[0]?.user?.fullName}</p>
