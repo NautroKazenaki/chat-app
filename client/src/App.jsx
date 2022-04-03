918a621a09829e0f527f50e8d14df8ded6853155
Сергей Ашарин <kalliak2014@yandex.ru>
2022-04-03 19:40:08 +0300

    server start + axios + auth w/ cookies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
-import {StreamChat} from 'stream-chat'
-import {Chat} from 'stream-chat-react'
-import Cookies from 'universal-cookie'
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
 
-import {ChannelListContainer, ChannelContainer, Auth} from './components'
+import { ChannelListContainer, ChannelContainer, Auth } from "./components";
 
-import './App.css'
+import "./App.css";
 
-const apiKey = "4j5bsgxyzh4v"
+const apiKey = "4j5bsgxyzh4v";
 
-const client = StreamChat.getInstance(apiKey)
+const client = StreamChat.getInstance(apiKey);
 
-const authToken = false
+const authToken = false;
 
 const App = () => {
+  if (!authToken) return <Auth />;
 
-    if(!authToken) return <Auth />
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+    </div>
+  );
+};
 
-    return (
-        <div className="app__wrapper">
-            <Chat client={client} theme="team light">
-                <ChannelListContainer />
-                <ChannelContainer />
-            </Chat>
-        </div>
-    )
-}
-
-export default App
+export default App;
--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,76 +1,60 @@
-import React from 'react'
-import {ChannelList, useChatContext} from 'stream-chat-react'
-import Cookies from 'universal-cookie'
+import React from "react";
+import { ChannelList, useChatContext } from "stream-chat-react";
+import Cookies from "universal-cookie";
 
-import {ChannelSearch, TeamChannelList, TeamChannelPreview} from './'
-import ChatIcon from '../assets/chat.png'
-import LogoutIcon from '../assets/logout.png'
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+import ChatIcon from "../assets/chat.png";
+import LogoutIcon from "../assets/logout.png";
 
 const SideBar = () => (
-    <div className="channel-list__sidebar">
-        <div className="channel-list__sidebar__icon1">
-            <div className="icon1__inner">
-                <img src={ChatIcon} alt="icon" width="30" />
-            </div>
-        </div>
-        <div className="channel-list__sidebar__icon2">
-            <div className="icon1__inner">
-                <img src={LogoutIcon} alt="logout" width="30" />
-            </div>
-        </div>
+  <div className="channel-list__sidebar">
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__inner">
+        <img src={ChatIcon} alt="icon" width="30" />
+      </div>
     </div>
-)
+    <div className="channel-list__sidebar__icon2">
+      <div className="icon1__inner">
+        <img src={LogoutIcon} alt="logout" width="30" />
+      </div>
+    </div>
+  </div>
+);
 
 const ChatHeader = () => (
-    <div className="channel-list__header">
-        <p className="channel-list__header__text">
-            Chat App by Nautro
-        </p>
-    </div>
-)
+  <div className="channel-list__header">
+    <p className="channel-list__header__text">Chat App by Nautro</p>
+  </div>
+);
 
 const ChannelListContainer = () => {
-    return (
-        <>
-            <SideBar />
-            <div className="channel-list__list__wrapper">
-                <ChatHeader />
-                <ChannelSearch />
-                <ChannelList 
-                    filters={{}}
-                    channelRenderFilterFn={() => {}}
-                    List={(listProps) => (
-                        <TeamChannelList 
-                            {...listProps}
-                            type="team"
-                        />
-                    )}
-                    Preview={(previewProps) => (
-                        <TeamChannelPreview 
-                            {...previewProps}
-                            type="team"
-                        />
-                    )}
-                />
-                <ChannelList 
-                    filters={{}}
-                    channelRenderFilterFn={() => {}}
-                    List={(listProps) => (
-                        <TeamChannelList 
-                            {...listProps}
-                            type="messaging"
-                        />
-                    )}
-                    Preview={(previewProps) => (
-                        <TeamChannelPreview 
-                            {...previewProps}
-                            type="messaging"
-                        />
-                    )}
-                />
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <SideBar />
+      <div className="channel-list__list__wrapper">
+        <ChatHeader />
+        <ChannelSearch />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => <TeamChannelList {...listProps} type="team" />}
+          Preview={(previewProps) => (
+            <TeamChannelPreview {...previewProps} type="team" />
+          )}
+        />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => (
+            <TeamChannelList {...listProps} type="messaging" />
+          )}
+          Preview={(previewProps) => (
+            <TeamChannelPreview {...previewProps} type="messaging" />
+          )}
+        />
+      </div>
+    </>
+  );
+};
 
-export default ChannelListContainer
+export default ChannelListContainer;
